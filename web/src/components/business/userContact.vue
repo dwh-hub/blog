1d77382a25ac472a2f6dcb837917d6d7348f4267
董文浩 <dwhandwp@163.com>
2019-09-08 10:55:42 +0800

    社交账号板块

--- web/src/components/business/userContact.vue
+++ web/src/components/business/userContact.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="user-contact" class="blog-item">
     <div class="user-contact-item">
-      <i></i>
+      <i class="contact_iconfont icon-qq"></i>
       <a class="contact-text">1160395060</a>
     </div>
     <div class="user-contact-item">
-      <i></i>
-      <a class="contact-text">dwh-hub</a>
-    </div>
-    <div class="user-contact-item">
-      <i></i>
+      <i class="contact_iconfont icon-email"></i>
       <a class="contact-text">dwhandwp@163.com</a>
     </div>
     <div class="user-contact-item">
-      <i></i>
-      <a class="contact-text">大花园</a>
+      <i class="contact_iconfont icon-github"></i>
+      <a class="contact-text" href="https://github.com/dwh-hub" target="_blank">dwh-hub</a>
+    </div>
+    <div class="user-contact-item">
+      <i class="contact_iconfont icon-bilibili"></i>
+      <a class="contact-text" href="https://space.bilibili.com/11500922" target="_blank">大花园</a>
     </div>
   </div>
 </template>
@@ -26,10 +26,23 @@ export default {
 </script>
 
 <style lang="less">
+@import "~COMMON/less/font.less";
 #user-contact {
   .user-contact-item {
     line-height: 44px;
     border-bottom: 1px solid #f0f0f0;
+    .contact_iconfont {
+      font-size: 18px;
+      vertical-align: middle;
+    }
+    .icon-bilibili {
+      font-size: 22px;
+      margin-left: -2px;
+    }
+    .contact-text {
+      margin-left: 5px;
+      vertical-align: middle;
+    }
   }
 }
 </style>
