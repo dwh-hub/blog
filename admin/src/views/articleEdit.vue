a9b357129cd384788b6b637e4b9633396f19b632
董文浩 <dwhandwp@163.com>
2019-10-18 15:08:54 +0800

    增加富文本编辑器

--- admin/src/views/articleEdit.vue
+++ admin/src/views/articleEdit.vue
@@ -12,11 +12,22 @@
           ></el-option>
         </el-select>
       </el-form-item>
+      <el-form-item label="编辑类型">
+        <el-select v-model="editorType" placeholder="请选择" @change="changeEditorType">
+          <el-option
+            v-for="item in editorTypes"
+            :key="item.value"
+            :label="item.type"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="标题">
         <el-input type="text" v-model="title"></el-input>
       </el-form-item>
       <el-form-item label="内容">
-        <mavon-editor ref="md" v-model="content" @imgAdd="$imgAdd" />
+        <mavon-editor ref="md" v-model="content" @imgAdd="$imgAdd" v-if="editorType == 1"/>
+        <tinymce-editor id="tinymce" v-model="contentHtml" :init="tinymceInit" v-if="editorType == 2"></tinymce-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="saveArticle()">保存</el-button>
@@ -29,6 +40,10 @@
 import { mavonEditor } from "mavon-editor";
 import "mavon-editor/dist/css/index.css";
 
+import tinymce from "tinymce/tinymce";
+import "tinymce/themes/silver/theme";
+import tinymceEditor from "@tinymce/tinymce-vue";
+
 export default {
   props: {
     id: {}
@@ -38,15 +53,34 @@ export default {
       parentOptions: [],
       tags: [],
       title: "",
-      content: ""
+      content: "",
+      contentHtml: "",
+      tinymceInit: {
+        language_url: '../../public/zh_CN.js',
+        language: 'zh_CN',
+        // skin_url: '../../public/skins/ui/oxide',
+        height: 300
+      },
+      editorTypes: [{
+        value: 1,
+        type: 'markdown'
+      },{
+        value: 2,
+        type: 'html'
+      }],
+      editorType: 1
     };
   },
   components: {
+    tinymceEditor,
     mavonEditor
   },
   created() {
     this.id && this.fetch();
     this.getTagList();
+  },
+  mounted() {
+    tinymce.init({})
   },
   methods: {
     async fetch() {
@@ -60,9 +94,9 @@ export default {
       this.parentOptions = res.data;
     },
     async $imgAdd(pos, $file) {
-      const formdata = new FormData()
-      formdata.append('file', $file);
-      const res = await this.$axios.post('/admin/api/upload', formdata)
+      const formdata = new FormData();
+      formdata.append("file", $file);
+      const res = await this.$axios.post("/admin/api/upload", formdata);
       this.$refs.md.$img2Url(pos, res.data);
     },
     async saveArticle() {
@@ -86,6 +120,9 @@ export default {
         type: "success"
       });
       this.$router.push({ path: "/article/list" });
+    },
+    changeEditorType() {
+      console.log(this.editorType)
     }
   }
 };
