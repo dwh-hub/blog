c0a08238f7cab4bfc967d55a0d07ee1d41caaa59
dongwenhao <dwhandwp@163.com>
2021-01-01 22:01:32 +0800

    去除打包文件，修改生产环境地址

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -2,7 +2,7 @@ export default {
     baseUrl: {
       development: "http://localhost:3000", // 本地环境,
       test: "http://localhost:3000", // 测试环境
-      production: "http://localhost:3000", // 生产环境
+      production: "", // 生产环境
     },
   };
   
