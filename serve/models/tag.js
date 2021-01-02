5121a3c286ee19bf02f7d237174b6e31db98dc02
dongwenhao <dwhandwp@163.com>
2021-01-02 10:25:57 +0800

    删除tag

--- serve/models/tag.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = new mongoose.Schema({
-  name: { type: String},
-  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Tag' }
-})
-
-module.exports = mongoose.model('Tag', schema)
