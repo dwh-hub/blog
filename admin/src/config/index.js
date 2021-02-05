a88356413abe400ec3f7593ba2b459dba6ed0b20
dongwenhao <dwhandwp@163.com>
2021-02-05 23:07:32 +0800

    生产环境处理

--- admin/src/config/index.js
+++ admin/src/config/index.js
@@ -4,7 +4,7 @@ export default {
     baseUrl: {
       development: "http://localhost:3000", // 本地环境,
       test: "http://localhost:3000", // 测试环境
-      production: "http://cdn.dahuayuan.cn", // 生产环境
+      production: "http://dahuayuan.cn", // 生产环境
     },
     qiniuUploadUrl: "https://upload-z2.qiniup.com", // 七牛云上传地址
     qiniuUrl: "http://cdn.dahuayuan.cn/" // 七牛云预览地址
